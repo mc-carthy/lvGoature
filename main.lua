a2f4b9209bd2f75bcb7e1ed1972e0375dac3ff3a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-21 18:12:28 +0100

    Draw basic environment

--- main.lua
+++ main.lua
@@ -1,13 +1,21 @@
-function love.load()
+local cloud = {}
 
+function love.load()
+    cloud.x = 128
+    cloud.xSpeed = 32
 end
 
 function love.update(dt)
-
+    cloud.x = cloud.x + cloud.xSpeed * dt
 end
 
 function love.draw()
-
+    love.graphics.setColor(191, 255, 255, 255)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight() / 2)
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.rectangle('fill', cloud.x, love.graphics.getHeight() / 4, love.graphics.getWidth() / 4, love.graphics.getHeight() / 8)
+    love.graphics.setColor(127, 159, 0, 255)
+    love.graphics.rectangle('fill', 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), love.graphics.getHeight() / 2)
 end
 
 function love.keypressed(key)
